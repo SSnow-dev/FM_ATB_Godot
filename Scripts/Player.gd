bf3752232ba61ef545967843c448f439b6b00272
Steven <ssnow86.dev@gmail.com>
2021-08-08 19:14:10 -0700

    Initial commit
    First commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+const MAX_SPEED = 120
+const ACCELERATION = 300
+const FRICTION = 300
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move_and_collide(velocity)
